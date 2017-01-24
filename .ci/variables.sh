1b4eaa4d2b09675993af1f551f56da48f70d22d9
Prateek Rungta <rungta@uber.com>
2017-01-24 16:23:47 -0500

    updating makefile, travis config

--- .ci/variables.sh
+++ .ci/variables.sh
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 export PACKAGE='github.com/m3db/m3cluster'
+=======
+>>>>>>> c867de7... updating makefile, travis config
 export GO15VENDOREXPERIMENT=1
 export VENDOR_PATH=$PACKAGE/vendor
 export LICENSE_BIN=$GOPATH/src/$PACKAGE/.ci/uber-licence/bin/licence
