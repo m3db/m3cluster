25d8d3df3a9591148cae65425845d57df006494b
Chao Wang <chaowang@uber.com>
2017-04-12 16:27:48 -0400

    improve test

--- kv/util/util_test.go
+++ kv/util/util_test.go
@@ -6,18 +6,18 @@ import (
 	"time"
 
 	"github.com/golang/mock/gomock"
-	"github.com/golang/protobuf/proto"
 	"github.com/m3db/m3cluster/generated/proto/commonpb"
 	"github.com/m3db/m3cluster/kv"
+	"github.com/m3db/m3cluster/kv/mem"
 	"github.com/stretchr/testify/require"
 )
 
 func TestBoolFromValue(t *testing.T) {
-	require.True(t, BoolFromValue(newMockValue(t, &commonpb.BoolProto{Value: true}), "key", false))
-	require.False(t, BoolFromValue(newMockValue(t, &commonpb.BoolProto{Value: false}), "key", true))
+	require.True(t, BoolFromValue(mem.NewValue(0, &commonpb.BoolProto{Value: true}), "key", false))
+	require.False(t, BoolFromValue(mem.NewValue(0, &commonpb.BoolProto{Value: false}), "key", true))
 
-	require.True(t, BoolFromValue(newMockValue(t, &commonpb.Float64Proto{Value: 123}), "key", true))
-	require.False(t, BoolFromValue(newMockValue(t, &commonpb.Float64Proto{Value: 123}), "key", false))
+	require.True(t, BoolFromValue(mem.NewValue(0, &commonpb.Float64Proto{Value: 123}), "key", true))
+	require.False(t, BoolFromValue(mem.NewValue(0, &commonpb.Float64Proto{Value: 123}), "key", false))
 
 	require.True(t, BoolFromValue(nil, "key", true))
 	require.False(t, BoolFromValue(nil, "key", false))
@@ -34,14 +34,14 @@ func TestUpdateBoolWithValue(t *testing.T) {
 	vw := kv.NewValueWatchable()
 	_, w, _ := vw.Watch()
 
-	vw.Update(newMockValue(t, &commonpb.BoolProto{Value: false}))
+	vw.Update(mem.NewValue(0, &commonpb.BoolProto{Value: false}))
 	<-w.C()
 	UpdateBoolWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, true, nil)
 	testConfig.Lock()
 	require.False(t, testConfig.v)
 	testConfig.Unlock()
 
-	vw.Update(newMockValue(t, &commonpb.BoolProto{Value: true}))
+	vw.Update(mem.NewValue(0, &commonpb.BoolProto{Value: true}))
 	<-w.C()
 	UpdateBoolWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, true, nil)
 	testConfig.Lock()
@@ -49,7 +49,7 @@ func TestUpdateBoolWithValue(t *testing.T) {
 	testConfig.Unlock()
 
 	// mimic a bad value update, should fall back to default
-	vw.Update(newMockValue(t, &commonpb.Float64Proto{Value: 20}))
+	vw.Update(mem.NewValue(0, &commonpb.Float64Proto{Value: 20}))
 	<-w.C()
 	UpdateBoolWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, true, nil)
 	testConfig.Lock()
@@ -66,8 +66,8 @@ func TestUpdateBoolWithValue(t *testing.T) {
 }
 
 func TestFloat64FromValue(t *testing.T) {
-	require.Equal(t, 20.5, Float64FromValue(newMockValue(t, &commonpb.Int64Proto{Value: 200}), "key", 20.5))
-	require.Equal(t, 123.3, Float64FromValue(newMockValue(t, &commonpb.Float64Proto{Value: 123.3}), "key", 20))
+	require.Equal(t, 20.5, Float64FromValue(mem.NewValue(0, &commonpb.Int64Proto{Value: 200}), "key", 20.5))
+	require.Equal(t, 123.3, Float64FromValue(mem.NewValue(0, &commonpb.Float64Proto{Value: 123.3}), "key", 20))
 	require.Equal(t, 20.1, Float64FromValue(nil, "key", 20.1))
 }
 
@@ -82,7 +82,7 @@ func TestUpdateFloat64WithValue(t *testing.T) {
 	vw := kv.NewValueWatchable()
 	_, w, _ := vw.Watch()
 
-	vw.Update(newMockValue(t, &commonpb.Float64Proto{Value: 20}))
+	vw.Update(mem.NewValue(0, &commonpb.Float64Proto{Value: 20}))
 	<-w.C()
 	UpdateFloat64WithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, 0, nil)
 	testConfig.Lock()
@@ -90,7 +90,7 @@ func TestUpdateFloat64WithValue(t *testing.T) {
 	testConfig.Unlock()
 
 	// mimic a bad value update, should fall back to default
-	vw.Update(newMockValue(t, &commonpb.BoolProto{Value: true}))
+	vw.Update(mem.NewValue(0, &commonpb.BoolProto{Value: true}))
 	<-w.C()
 	UpdateFloat64WithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, 30, nil)
 	testConfig.Lock()
@@ -107,8 +107,8 @@ func TestUpdateFloat64WithValue(t *testing.T) {
 }
 
 func TestInt64FromValue(t *testing.T) {
-	require.Equal(t, int64(200), Int64FromValue(newMockValue(t, &commonpb.Int64Proto{Value: 200}), "key", 20))
-	require.Equal(t, int64(20), Int64FromValue(newMockValue(t, &commonpb.Float64Proto{Value: 123}), "key", 20))
+	require.Equal(t, int64(200), Int64FromValue(mem.NewValue(0, &commonpb.Int64Proto{Value: 200}), "key", 20))
+	require.Equal(t, int64(20), Int64FromValue(mem.NewValue(0, &commonpb.Float64Proto{Value: 123}), "key", 20))
 	require.Equal(t, int64(20), Int64FromValue(nil, "key", 20))
 }
 
@@ -123,7 +123,7 @@ func TestUpdateInt64WithValue(t *testing.T) {
 	vw := kv.NewValueWatchable()
 	_, w, _ := vw.Watch()
 
-	vw.Update(newMockValue(t, &commonpb.Int64Proto{Value: 20}))
+	vw.Update(mem.NewValue(0, &commonpb.Int64Proto{Value: 20}))
 	<-w.C()
 	UpdateInt64WithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, 0, nil)
 	testConfig.Lock()
@@ -131,7 +131,7 @@ func TestUpdateInt64WithValue(t *testing.T) {
 	testConfig.Unlock()
 
 	// mimic a bad value update, should fall back to default
-	vw.Update(newMockValue(t, &commonpb.Float64Proto{Value: 100}))
+	vw.Update(mem.NewValue(0, &commonpb.Float64Proto{Value: 100}))
 	<-w.C()
 	UpdateInt64WithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, 30, nil)
 	testConfig.Lock()
@@ -151,8 +151,8 @@ func TestStringArrayFromValue(t *testing.T) {
 	defaultValue := []string{"d1", "d2"}
 	v1 := []string{"s1", "s2"}
 
-	require.Equal(t, v1, StringArrayFromValue(newMockValue(t, &commonpb.StringArrayProto{Values: v1}), "key", defaultValue))
-	require.Equal(t, defaultValue, StringArrayFromValue(newMockValue(t, &commonpb.Float64Proto{Value: 123}), "key", defaultValue))
+	require.Equal(t, v1, StringArrayFromValue(mem.NewValue(0, &commonpb.StringArrayProto{Values: v1}), "key", defaultValue))
+	require.Equal(t, defaultValue, StringArrayFromValue(mem.NewValue(0, &commonpb.Float64Proto{Value: 123}), "key", defaultValue))
 	require.Equal(t, defaultValue, StringArrayFromValue(nil, "key", defaultValue))
 }
 
@@ -170,7 +170,7 @@ func TestUpdateStringArrayWithValue(t *testing.T) {
 	defaultValue := []string{"d1", "d2"}
 
 	v1 := []string{"s1", "s2"}
-	vw.Update(newMockValue(t, &commonpb.StringArrayProto{Values: v1}))
+	vw.Update(mem.NewValue(0, &commonpb.StringArrayProto{Values: v1}))
 	<-w.C()
 	UpdateStringArrayWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, defaultValue, nil)
 	testConfig.Lock()
@@ -178,7 +178,7 @@ func TestUpdateStringArrayWithValue(t *testing.T) {
 	testConfig.Unlock()
 
 	// mimic a bad value update, should fall back to default
-	vw.Update(newMockValue(t, &commonpb.Float64Proto{Value: 100}))
+	vw.Update(mem.NewValue(0, &commonpb.Float64Proto{Value: 100}))
 	<-w.C()
 	UpdateStringArrayWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, defaultValue, nil)
 	testConfig.Lock()
@@ -194,7 +194,7 @@ func TestUpdateStringArrayWithValue(t *testing.T) {
 	testConfig.Unlock()
 
 	// test nil default value
-	vw.Update(newMockValue(t, &commonpb.Float64Proto{Value: 100}))
+	vw.Update(mem.NewValue(0, &commonpb.Float64Proto{Value: 100}))
 	<-w.C()
 	UpdateStringArrayWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, nil, nil)
 	testConfig.Lock()
@@ -216,7 +216,7 @@ func TestUpdateTimeWithValue(t *testing.T) {
 	defaultTime := time.Now().Add(time.Hour)
 	t1 := time.Now()
 
-	vw.Update(newMockValue(t, &commonpb.Int64Proto{Value: t1.Unix()}))
+	vw.Update(mem.NewValue(0, &commonpb.Int64Proto{Value: t1.Unix()}))
 	<-w.C()
 	UpdateTimeWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, defaultTime, nil)
 	testConfig.Lock()
@@ -224,7 +224,7 @@ func TestUpdateTimeWithValue(t *testing.T) {
 	testConfig.Unlock()
 
 	// mimic a bad value update, should fall back to default
-	vw.Update(newMockValue(t, &commonpb.Float64Proto{Value: 20}))
+	vw.Update(mem.NewValue(0, &commonpb.Float64Proto{Value: 20}))
 	<-w.C()
 	UpdateTimeWithValue(w.Get(), "foo", &testConfig.v, &testConfig.RWMutex, defaultTime, nil)
 	testConfig.Lock()
@@ -238,25 +238,4 @@ func TestUpdateTimeWithValue(t *testing.T) {
 	testConfig.Lock()
 	require.Equal(t, defaultTime, testConfig.v)
 	testConfig.Unlock()
-}
-
-type mockValue struct {
-	data []byte
-}
-
-func newMockValue(t *testing.T, msg proto.Message) *mockValue {
-	b, err := proto.Marshal(msg)
-	require.NoError(t, err)
-
-	return &mockValue{data: b}
-}
-
-func (mv *mockValue) Unmarshal(msg proto.Message) error {
-	return proto.Unmarshal(mv.data, msg)
-}
-
-func (mv *mockValue) Version() int { return 0 }
-
-func (mv *mockValue) IsNewer(other kv.Value) bool {
-	return mv.Version() > other.Version()
 }
