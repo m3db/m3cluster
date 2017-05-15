b431a3aadf1187f2aac436f6ebc3285863f4200b
Matt Schallert <mschalle@uber.com>
2017-05-15 16:45:35 -0400

    [leader] Increase Leader() ctx timeout to 30s

--- services/leader/service.go
+++ services/leader/service.go
@@ -101,7 +101,9 @@ func (c *client) Resign() error {
 }
 
 func (c *client) Leader() (string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// TODO(mschalle): make configurable. need greater than client's DialTimeout
+	// to be resilient to node failures
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	return c.election.Leader(ctx)
 }
